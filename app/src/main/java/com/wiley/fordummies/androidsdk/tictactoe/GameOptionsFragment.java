d9111716f3574c70a65f7cf00fc96c0390e4697d
acchampion <championone@gmail.com>
2019-10-04 10:36:04 -0400

    Updated Gradle for Android Studio 3.5.1

--- app/src/main/java/com/wiley/fordummies/androidsdk/tictactoe/GameOptionsFragment.java
+++ app/src/main/java/com/wiley/fordummies/androidsdk/tictactoe/GameOptionsFragment.java
@@ -3,11 +3,6 @@ package com.wiley.fordummies.androidsdk.tictactoe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import timber.log.Timber;
 
 /**
